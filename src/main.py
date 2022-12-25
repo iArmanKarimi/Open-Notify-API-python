e8e86d566ff05c78d68e922b2a62232777360454
Arman Karimi <mrevid2@gmail.com>
2022-12-26 01:46:29 +0330

    ISS: convert json + type declaration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import json
+from typing import List
 from urllib import request
 from types import MappingProxyType
+from datetime import datetime
+from dataclasses import dataclass
 
 URLS = MappingProxyType(
     {
@@ -8,6 +11,33 @@ URLS = MappingProxyType(
         "ISS_NOW": "http://api.open-notify.org/iss-now.json",
     }
 )
+
+
+@dataclass
+class Location:
+    latitude: float
+    longitude: float
+
+
+@dataclass
+class ISSLocation:
+    message: str
+    date_time: datetime
+    utc_date_time: datetime
+    location: Location
+
+
+@dataclass
+class People:
+    name: str
+    craft: str
+
+
+@dataclass
+class PeopleInSpace:
+    number: int
+    message: str
+    people: List[People]
 
 
 class OpenNotify:
@@ -23,10 +53,22 @@ class OpenNotify:
         return data
 
     @staticmethod
-    def get_ISS_location():
+    def get_ISS_location() -> ISSLocation:
         data = OpenNotify.__get(URLS['ISS_NOW'])
         data = json.loads(data)
-        return data
+        unix = data['timestamp']
+
+        message = data['message']
+        date_time = datetime.fromtimestamp(unix)
+        utc_date_time = datetime.utcfromtimestamp(unix)
+        latitude = data['iss_position']['latitude']
+        longitude = data['iss_position']['longitude']
+        location = Location(latitude=latitude, longitude=longitude)
+        del data
+        return ISSLocation(
+            location=location, message=message,
+            date_time=date_time, utc_date_time=utc_date_time
+        )
 
     @staticmethod
     async def get_people_in_space_async():
